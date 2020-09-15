8ddea9f7b43024b35bcd1edf8d175834ec718ed2
Aaron Amodt <aaron@amodttech.com>
2020-09-15 00:16:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,13 +23,13 @@ def move(board, index, current_player = "X")
   board[index] = current_player
 end
 
-
+def input_to_index(input)
+  input.to_i - 1
+end
 
 def turn(board)
   puts "Please Enter 1-9:"
-  def input_to_index(input)
-  input.to_i - 1
-  end
+
 end
 
 
