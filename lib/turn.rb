67100bbf1944aca8f15cf357227bc4c1f09e2824
Aaron Amodt <aaron@amodttech.com>
2020-09-15 01:05:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,11 +6,11 @@ def display_board(board)
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
   
-  def valid_move?(board, index)
+def valid_move?(board, index)
     index.between?(0,8) != position_taken?(board, index)
   end
   
-  def position_taken?(board, index)
+def position_taken?(board, index)
     taken = nil
     if (board[index] == " " || board[index] == "" || board[index] == nil)
       taken = false
@@ -19,15 +19,15 @@ def display_board(board)
     end
   end
   
-  def move(board, index, current_player = "X")
+def move(board, index, current_player = "X")
     board[index] = current_player
   end
   
-  def input_to_index(user_input)
+def input_to_index(user_input)
     user_input.to_i - 1
   end
   
-  def turn(board)
+def turn(board)
     puts "Please enter 1-9:"
     input = gets
     index = input_to_index(input)
