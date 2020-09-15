d88e881cea9a91c650bf648b2146a465e2e4a6ad
Aaron Amodt <aaron@amodttech.com>
2020-09-15 00:20:13 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,11 +25,14 @@ end
 
 def input_to_index(input)
   input.to_i - 1
+  input = index
 end
 
 def turn(board)
   puts "Please enter 1-9:"
   input = gets
+  input_to_index(input)
+  valid_move?(board, index)
 end
 
 
